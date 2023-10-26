88d525ddb48facc76255b3390ab13cd0b35e1b4a
rtjshreyd <shreyanshece1041@gmail.com>
2023-10-27 02:32:06 +0530

    building up

---
+++ medical_reception/main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI
+import redis
+import uvicorn
+
+app = FastAPI()
+
+# Initialize Redis and RabbitMQ (pika) connections
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# Endpoint to initialize resources
+@app.post("/initialise")
+async def initialize_resources():
+    """
+    Initialize resources.
+    
+    This endpoint initializes resources.
+
+    Returns:
+        dict: Empty dictionary.
+    """
+    try:
+        # Initialize resources here
+        # ...
+        return {}
+    except Exception as e:
+        # Handle exceptions here
+        return {}
+
+
+
+# Endpoint for chat functionality
+@app.post("/chat")
+async def chat_functionality():
+    """
+    Implement chat functionality.
+    
+    This endpoint implements chat functionality.
+
+    Returns:
+        dict: Empty dictionary.
+    """
+    try:
+        # Implement chat functionality here
+        # ...
+        return {}
+    except Exception as e:
+        # Handle exceptions here
+        return {}
+
+
+
+# Endpoint to close resources
+@app.post("/close")
+async def close_resources():
+    """
+    Close resources.
+    
+    This endpoint closes resources.
+
+    Returns:
+        dict: Empty dictionary.
+    """
+    try:
+        # Close resources here
+        # ...
+        return {}
+    except Exception as e:
+        # Handle exceptions here
+        return {}
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+
+
+# run - uvicorn main:app --reload --port 8001
