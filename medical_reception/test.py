c65da911cc0a5e9be83c7d6b5c4354628f2ed4d7
rtjshreyd <shreyanshece1041@gmail.com>
2023-10-30 00:34:01 +0530

    perfect test

--- medical_reception/test.py
+++ medical_reception/test.py
@@ -5,6 +5,15 @@ agent_instance = MedicalReceptionAgent()
 
 session_id = str(uuid.uuid4())
 
-resp = agent_instance.agent_begin(session_id)
+print("Session ID: " + session_id)
 
+resp = agent_instance.agent_begin(session_id)
+print(resp)
+
+message = "Hi Uday, I have some fever please recommend me a doctor"
+resp = agent_instance.agent_chat(session_id, message)
+print(resp)
+
+message = "Please make my booking with any doctor at the earliest possible time today"
+resp = agent_instance.agent_chat(session_id, message)
 print(resp)
