8ab333cc954a68855ddb235784af40a73e81c279
rtjshreyd <shreyanshece1041@gmail.com>
2023-12-04 23:26:27 +0530

    manual testing

--- medical_reception/test_worker.py
+++ medical_reception/test_worker.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from workers.operations import operator  
+from workers.operations import operator, ask_agent
 from dotenv import load_dotenv
 import os
 
@@ -11,17 +11,19 @@ app.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 app.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
 obj = {
-    'session_id': "123456",
+    'session_id': "7a76d2",
+    'call_sid' : "7a76d2",
+    'query': "I have fever recommend me some doctor"
 }
 
 
 def publish_task(input_dict):
     try:
-        result = operator.apply_async(args=[input_dict])
+        result = ask_agent.apply_async(args=[input_dict])
         print(f"Task {result.id} published successfully.")
     except Exception as e:
         print(f"Failed to publish task: {e}")
 
-# Example usage
+
 if __name__ == "__main__":
     publish_task(obj)
